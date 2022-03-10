021241fb62a185ce83700b4410237b03f337f615
Mangut <mangutjikmyan@gmail.com>
2022-03-10 13:45:11 +0100

    Created new branch

--- transformer.py
+++ transformer.py
@@ -1,3 +1,4 @@
+from wsgiref.handlers import format_date_time
 import pandas as pd
 from get_data import get_data
 from save_data import save_data
@@ -11,3 +12,19 @@ def transform_data(df: pd.DataFrame)-> pd.DataFrame:
     # sort data
     # filter data
     pass
+def remove_special_characters(text):
+    """ Remove special characters from a string.
+    Args:
+        text (str): Source text to remove special characters.
+    Raises:
+        Exception: Any other error message
+    Returns:
+        [str]: Text with special characters removed.
+    """
+    txt = ""
+    try:
+        txt = text.translate({ord(c): "_" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
+        txt = txt.replace(' ', '_')
+    except Exception as e:
+        print('remove_special_characters: ' + repr(e))
+    return txt
