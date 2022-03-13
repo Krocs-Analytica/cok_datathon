f39455839de0c6fa9394e0d64d8c4b4287f1f147
Oluwasina Peter Adebiyi <adebiyi_sina@yahoo.com>
2022-03-13 17:04:10 +0100

    This is the save_data function to be used after get_data and data_validator

--- save_data.py
+++ save_data.py
@@ -1,4 +1,31 @@
 import pandas as pd
 
-def save_data(df: pd.DataFrame, path: str, source: str) -> None:
-    pass
+df = pd.read_csv(r'C:\Users\USER\Documents\All_Docs\ClickOnKaduna\Field Activity\Task\HR_Complete..csv')
+df.head()
+
+def ensure_directory_exist(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def save_data(df: pd.DataFrame, filename: str, path: str, source: str, allow_index: bool = False) -> None:
+    try:
+        if not path:
+            path = 'output'
+
+        ensure_directory_exist(path)
+        
+        if source == 'csv':
+            if '.csv' not in filename:
+                filename = filename.replace(' ', '_') + '.csv'
+                
+            long_file_name = os.path.join(path, filename)
+            df.to_csv(path_or_buf=long_file_name, index=allow_index)
+            
+        elif source == 'excel':
+            if '.xls' not in filename:
+                filename = filename.replace(' ', '_') + '.xlsx'
+                
+            long_file_name = os.path.join(path, filename)
+            df.to_excel(path_or_buf=long_file_name, index=allow_index)
+    except Exception as e:
+        print(e)        
